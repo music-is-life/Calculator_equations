379cee9580b077b44062f82bb8e436203dea2c04
music-is-life <daniel.natan101@gmail.com>
2021-03-23 01:01:49 +0200

    second

--- equations.py
+++ equations.py
@@ -19,7 +19,8 @@ def pow (x:float, y:int):
         sum = x
         for i in range(1,y):
             sum = sum * x
-        return sum
+        a = float('%0.6f' % sum)
+        return a
 
 
 
@@ -30,7 +31,8 @@ def exponent (x:float):
     while (absolut_value((x)/fuctorial(i)) > 0.00000000000000000000000000000000000000000001) & (i <= 5000000000):
         sum += (pow(x, i))/(fuctorial(i))
         i += 1
-    return sum
+    a = float('%0.6f' % sum)
+    return a
 
 
 #forth function is Ln:
@@ -41,11 +43,13 @@ def Ln (x:float):
         while True:
             y_2 = y_0 + 2 * (x - exponent(y_0))/(x + exponent(y_0))
             if absolut_value(y_0 - y_2) <= 0.001:
-                return y_0
+                y_0_r = float('%0.6f' % y_0)
+                return y_0_r
             y_0 = y_2
     else:
         return 0
-        
+
+
 #ln_value = float('%0.6f' % Ln(5.0))
 #print(ln_value)
 
@@ -53,11 +57,12 @@ def Ln (x:float):
 
 #the FIRST MAIN function XtimesY:
 def XtimesY(x:float, y:float):
-        if x >= 0:
-            return (exponent(y * Ln(x)))
+        if x > 0:
+            a = (exponent(y * Ln(x)))
+            b = float('%0.6f' % a)
+            return b
         else:
             return 0        
-        
 
 
 
@@ -69,8 +74,9 @@ def sqrt (x:float, y:float):
 
 #final function calculate formula:
 def calculate (x:float):
-    if x>=0:
-        return (exponent(x) * XtimesY(7.0, x) * XtimesY(x, -1.0) * sqrt(x, x))
+    if x>0:
+        a = exponent(x) * XtimesY(7.0, x) * XtimesY(x, -1.0) * sqrt(x, x)
+        b = float('%0.6f' % a)
+        return b
     else:
         return 0.0
-    
